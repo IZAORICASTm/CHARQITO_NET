6975a28ba9bf2b37b7551b4e813be9a74adb08ec
Cloro <izaoricas@alu.edu.gva.es>
2024-05-24 13:48:32 +0200

    Update mamasita.sh

--- CHARQITO_NET/mamasita.sh
+++ CHARQITO_NET/mamasita.sh
@@ -1,203 +1,53 @@
 #!/bin/bash
-while true
-do
-clear
-echo CHARQUITO
-echo
-sleep 1
-echo la red social de bash
-echo
-echo -n " necesitas iniciar sesion. quieres iniciar sesion? (S/N) : "
-read log
-case $log in
-S)
-	echo -n "tienes cuenta, manito? (S/N)"
-	read paps
-	case $paps in
-		S)
-		clear
-			bash login.sh
-			;;
-		N)
-		clear
-			bash signup.sh
-			;;
-		*)
-                echo UIUIUI QUE TAS COLAO
-                sleep
-			mkdir $paps
-			;;
-	esac
-;;
-N)
-	echo ok xd
-	sleep 3.69
-	poweroff
-;;
 
-ELOY)
-	sleep 4
-	echo -n cargando
-	sleep 0.7
-	echo -n .
-	sleep 1.3
-	echo -n .
-	sleep 1.95
-	echo -n .
-	sleep 2.46
-	echo -n .
-	sleep 0.2
-	echo -n . 
-        sleep 0.2
-	echo -n . 
-        sleep 0.2
-	echo -n . 
-        sleep 0.2
-	echo -n . 
-        sleep 0.2
-	echo . 
-        sleep 0.2
-	echo . 
-        sleep 0.2
-	echo . 
-        sleep 0.2
-	echo . 
-        sleep 0.2
-	echo . 
-        sleep 0.2
-	echo . 
-        sleep 0.2
-	echo . 
-	sleep 0.01
-	echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.01
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        echo .
-        sleep 0.0001
-        clear
-        bash sexo.sh
-	;;
+while true; do
+    clear
+    echo "CHARQUITO"
+    echo
+    sleep 1
+    echo "La red social de Bash"
+    echo
+    read -p "¿Necesitas iniciar sesión? (S/N): " log
 
-*)
-	echo puta mare aixó que es?
-	sleep 0.75
-	echo presiona enter para  salir
-	read 
-	clear
-	exit
-	;;
-esac
+    case $log in
+        [Ss]*)
+            read -p "¿Tienes cuenta? (S/N): " paps
+            case $paps in
+                [Ss]*)
+                    clear
+                    bash login.sh
+                    ;;
+                [Nn]*)
+                    clear
+                    bash signup.sh
+                    ;;
+                *)
+                    echo "¡Ups! ¿Qué pasó?"
+                    sleep 2
+                    ;;
+            esac
+            ;;
+        [Nn]*)
+            echo "¡Ok! Nos vemos luego."
+            sleep 3
+            poweroff
+            ;;
+        ELOY)
+            echo "Cargando..."
+            sleep 2
+            echo -n "............"
+            sleep 0.5
+            echo -n "............"
+            sleep 0.5
+            echo -n "............"
+            sleep 0.5
+            clear
+            bash sexo.sh
+            ;;
+        *)
+            echo "¿Qué? No entendí."
+            sleep 1
+            ;;
+    esac
 done
+
