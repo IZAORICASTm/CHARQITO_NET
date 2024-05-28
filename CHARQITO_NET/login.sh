7e31351bfb1e2d0e3f3c6201a42d79eeea9cfcff
Cloro <izaoricas@alu.edu.gva.es>
2024-05-28 11:31:02 +0200

    error fix

--- CHARQITO_NET/login.sh
+++ CHARQITO_NET/login.sh
@@ -5,7 +5,7 @@ while true; do
     # Definir las respuestas correctas y sus respectivos acertijos
     declare -A riddles
     riddles=(
-        ["sombra"]="Todas las cosas tienen este, algunas tienen una, pero ninguna tiene dos. ¿Qué es?"
+        ["sombra"]="Todas las cosas tienen esto, algunas tienen una, pero ninguna tiene dos. ¿Qué es?"
         ["silencio"]="Es más ligero que una pluma, pero incluso el más fuerte no puede sostenerlo por mucho tiempo. ¿Qué es?"
         ["reloj"]="Lo haces correr todo el día, lo haces correr todo el año, pero nunca puedes tocarlo. ¿Qué es?"
         ["agua"]="Mientras más seca, más agua tiene. ¿Qué es?"
@@ -15,11 +15,12 @@ while true; do
         ["eco"]="Responde sin ser preguntado. ¿Qué es?"
         ["secreto"]="Lo guardas, pero cuando lo compartes, lo pierdes. ¿Qué es?"
         ["oscuridad"]="Siempre está detrás de ti y nunca podrás escapar de ella. ¿Qué es?"
-        ["net"]="El charqito esta brut o net?" 
+        ["net"]="El charqito esta brut o net?"
     )
 
-    # Seleccionar un acertijo aleatorio
-    respuesta_correcta=${!riddles[$(shuf -n1 -e "${!riddles[@]}")]}
+    # Seleccionar una respuesta aleatoria
+    respuestas=("${!riddles[@]}")
+    respuesta_correcta=${respuestas[$RANDOM % ${#respuestas[@]}]}
 
     # Solicitar al usuario que descifre el acertijo para acceder
     echo "Acertijo:"
