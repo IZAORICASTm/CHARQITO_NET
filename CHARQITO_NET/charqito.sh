2e365372df2ca83288c4e4220357b066408ffc7c
Cloro <izaoricas@alu.edu.gva.es>
2024-05-29 08:47:40 +0200

    Update charqito.sh for real jeez

--- CHARQITO_NET/charqito.sh
+++ CHARQITO_NET/charqito.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 while true
 do
+clear
 echo 
 echo
 echo el charqito net consiste en una red social
@@ -8,12 +9,12 @@ sleep 1
 echo el charqito net ha sido empezado en marzo o abril, yo que se
 sleep 1 
 echo el charqito net se ha hecho no usando el chatgpt
-sleep 0.25 
+sleep 0.25
 echo bueno 2 o 3 veces
 sleep 1
 echo el charqito net ha sido hecho por una persona
-echo 0.5
-echo es el gilipoyas que lo está presentando
+sleep 0.5
+echo  es el gilipoyas que lo está presentando
 sleep 1
 echo enter para continuar
 read
