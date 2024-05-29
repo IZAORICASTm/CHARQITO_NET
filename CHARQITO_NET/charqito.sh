8acefc68090d0e3a62095fc05f580f692ffc6161
Cloro <izaoricas@alu.edu.gva.es>
2024-05-29 08:40:27 +0200

    Create charqito.sh
    for school presentation

---
+++ CHARQITO_NET/charqito.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+while true do
+echo 
+echo
+echo el charqito net consiste en una red social
+sleep 1
+echo el charqito net ha sido empezado en marzo o abril, yo que se
+sleep 1 
+echo el charqito net se ha hecho no usando el chatgpt
+sleep 0.25 bueno 2 o 3 veces
+sleep 1
+echo el charqito net ha sido hecho por una persona
+echo 0.5
+echo (es el gilipoyas que lo está presentando)
+sleep 1
+echo enter para continuar
+read
+bash question.sh
+done
